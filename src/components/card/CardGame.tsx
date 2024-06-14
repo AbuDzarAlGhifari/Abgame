94dfd9ce8664a2c0917726d5d34786d3ed3dc439
AbuDzarAlGhifari <rasduba@gmail.com>
2024-06-14 22:25:47 +0700

    update:card

--- src/components/card/CardGame.tsx
+++ src/components/card/CardGame.tsx
@@ -1,16 +1,8 @@
-import { Tooltip } from "@material-tailwind/react";
-
 import Typography from "@/components/ui/Typography";
 
 export function BlogCard() {
     return (
         <section className='max-w-[24rem] overflow-hidden'>
-            <div color='transparent' className='m-0 rounded-none'>
-                <img
-                    src='https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80'
-                    alt='ui/ux review check'
-                />
-            </div>
             <div>
                 <Typography variant='h4' color='blue-gray'>
                     UI/UX Review Check
